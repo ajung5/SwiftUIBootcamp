5dbc8865278bd10871e65ea4a7687e2ba8f53d6f
Agung Nawawi <agung@fairtech.com.sg>
2023-09-05 21:13:05 +0700

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
