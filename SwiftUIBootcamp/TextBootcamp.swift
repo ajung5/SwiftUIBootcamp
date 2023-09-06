985d1e27f4e565dd5baf99a6806373016162c30f
Agung Nawawi <agung@fairtech.com.sg>
2023-09-06 09:26:12 +0700

    text - chaning font

--- SwiftUIBootcamp/TextBootcamp.swift
+++ SwiftUIBootcamp/TextBootcamp.swift
@@ -13,6 +13,7 @@ struct TextBootcamp: View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         // modifier
             .font(.title)
+            .fontWeight(.bold)
     }
 }
 
