130fab6047319a5e40bb065e454a9a152c7e1b3a
Agung Nawawi <agung@fairtech.com.sg>
2023-09-09 07:24:56 +0700

    rename background

---
+++ SwiftUIBootcamp/BackgroundAndOverlay/Overlay.swift
@@ -0,0 +1,20 @@
+//
+//  Overlay.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 09/09/23.
+//
+
+import SwiftUI
+
+struct Overlay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Overlay_Previews: PreviewProvider {
+    static var previews: some View {
+        Overlay()
+    }
+}
