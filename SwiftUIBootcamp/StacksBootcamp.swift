286bc1f1b36ea15f5a4cc5841df2ff75217dad09
Agung Nawawi <agung@fairtech.com.sg>
2023-09-11 11:09:38 +0700

    HStack dan ZStack

--- SwiftUIBootcamp/StacksBootcamp.swift
+++ SwiftUIBootcamp/StacksBootcamp.swift
@@ -16,6 +16,10 @@ struct StacksBootcamp: View {
     var body: some View {
         // Vstack
         VStack {
+            Text("VStack -> Virtual Stack")
+                .font(.headline)
+                .fontWeight(.bold)
+                .padding(.init(top: 20, leading: 10, bottom: 0, trailing: 9)) // custom padding pada tiap sisi
             Circle()
                 .fill(Color.red)
                 .frame(width: 100, height: 100)
@@ -27,16 +31,40 @@ struct StacksBootcamp: View {
                 .frame(width: 100, height: 100)
             
             // Hstack -> Hoorizontal
-            HStack{
+            VStack {
+                Text("HStack -> Horizontal Stack")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .padding(.init(top: 20, leading: 10, bottom: 0, trailing: 9)) // custom padding pada tiap sisi
+                    
+                HStack{
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 100, height: 100)
+                    Circle()
+                        .fill(Color.yellow)
+                        .frame(width: 100, height: 100)
+                    Circle()
+                        .fill(Color.green)
+                        .frame(width: 100, height: 100)
+                }
+            }
+            Text("ZStack -> zIndex Stack")
+                .font(.headline)
+                .fontWeight(.bold)
+                .padding(.init(top: 20, leading: 10, bottom: 0, trailing: 9)) // custom padding pada tiap sisi
+            ZStack{
+                // Dalam zstack, komponen paling bawah berada diposisi paling depan dalam layer
                 Circle()
                     .fill(Color.red)
-                    .frame(width: 100, height: 100)
+                    .frame(width: 150, height: 150)
                 Circle()
                     .fill(Color.yellow)
-                    .frame(width: 100, height: 100)
+                    .frame(width: 120, height: 120)
                 Circle()
                     .fill(Color.green)
-                    .frame(width: 100, height: 100)
+                    .frame(width: 90, height: 90)
             }
         }
     }
