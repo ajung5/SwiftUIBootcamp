dffe377c5a86656ea45b9e152d68c0a1d91c8c1b
Agung Nawawi <agung@fairtech.com.sg>
2023-09-12 15:13:05 +0700

    Padding ()

---
+++ SwiftUIBootcamp/PaddingAndSpacer/Padding.swift
@@ -0,0 +1,52 @@
+//
+//  Padding.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 12/09/23.
+//
+
+import SwiftUI
+
+struct Padding: View {
+    var body: some View {
+        
+        // dengan menggunakan padding, jadi ga perlu lagi pake frame. bisa lebih dynamic
+        VStack (alignment: .leading) {
+            // text title
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                //.underline(true, color: .gray)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            
+                // Menambahkan underline
+                .background(
+                    Color.gray
+                        .frame(height: 5)
+                        .offset(y: 14)
+                )
+                
+            
+            // description text
+            Text("These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided")
+                .foregroundColor(.secondary)
+        }
+        
+        .padding()
+        .padding(.vertical, 10)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(color: .black.opacity(0.3),
+                        radius: 10,
+                        x: 0.0, y: 10)
+        )
+        .padding(.horizontal, 55)
+    }
+}
+
+struct Padding_Previews: PreviewProvider {
+    static var previews: some View {
+        Padding()
+    }
+}
---
+++ SwiftUIBootcamp/PaddingAndSpacer/Spacer.swift
@@ -0,0 +1,20 @@
+//
+//  Spacer.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 12/09/23.
+//
+
+import SwiftUI
+
+struct Spacer: View {
+    var body: some View {
+        Text("asds")
+    }
+}
+
+struct Spacer_Previews: PreviewProvider {
+    static var previews: some View {
+        Spacer()
+    }
+}
