a408a5cab16c35c72f1a03760212d33d3b76b1d5
Agung Nawawi <agung@fairtech.com.sg>
2023-09-25 20:33:42 +0700

    Using Scroll View

--- SwiftUIBootcamp/SafeAreaBootcamp.swift
+++ SwiftUIBootcamp/SafeAreaBootcamp.swift
@@ -9,7 +9,68 @@ import SwiftUI
 
 struct SafeAreaBootcamp: View {
     var body: some View {
+        
+        /*
         ZStack {
+            
+            Color.teal
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView {
+                VStack {
+                    Text("Ini adalah judul")
+                        .font(.largeTitle)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    ForEach(0..<10) { index in
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .fill(.white)
+                            .frame(height: 150)
+                            .shadow(radius: 10)
+                            .padding(20)
+                    }
+                }
+            }
+        } */
+        
+        
+        
+        // # Cara 2, tidak menggunakab ZStack, tapi langsung di dalam background
+        
+        
+        ScrollView {
+            VStack {
+                Text("Ini adalah judul")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                ForEach(0..<10) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(.white)
+                        .frame(height: 150)
+                        .shadow(radius: 10)
+                        .padding(20)
+                }
+            }
+        }
+        .background {
+            Color.teal
+                .ignoresSafeArea()
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        ZStack {
+            // daripada membuat padding pada konten tsb untuk berada di safe area.
+            //  mending menggunakan ZStack
             
             // background
             Color.teal
@@ -26,7 +87,7 @@ struct SafeAreaBootcamp: View {
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             
-        }
+        } */
     }
 }
 
@@ -36,5 +97,4 @@ struct SafeAreaBootcamp_Previews: PreviewProvider {
     }
 }
 
-// daripada membuat padding pada konten tsb untuk berada di safe area.
-//  mending menggunakan ZStack
+
