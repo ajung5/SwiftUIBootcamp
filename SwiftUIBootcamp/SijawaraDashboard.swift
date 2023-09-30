88a70e65eee1e6bca958f5e351e4b5abb1112634
Agung Nawawi <agung@fairtech.com.sg>
2023-09-30 20:46:05 +0700

    presensii section

--- SwiftUIBootcamp/SijawaraDashboard.swift
+++ SwiftUIBootcamp/SijawaraDashboard.swift
@@ -59,16 +59,61 @@ struct topMenu: View {
 
 //  presensi dan account summary
 struct accountSummary: View {
+    
+    let kolom: [GridItem] = [
+        GridItem(.fixed(35), spacing: nil, alignment: .center),
+        GridItem(.fixed(175), spacing: nil, alignment: .leading),
+        GridItem(.flexible(), spacing: nil, alignment: .center)
+    ]
+    
     var body: some View {
         VStack {
             
+            // Presensi Section
             ZStack {
                 RoundedRectangle(cornerRadius: 10)
                     .fill(.white)
-                    .frame(height: 75)
+                    .frame(height: 50)
+                
+                LazyVGrid(columns: kolom) {
+                    Image(systemName: "clock")
+                        .font(.system(size: 22))
+                        .padding(.leading, 5)
+                    
+                    VStack (alignment: .leading) {
+                        Text("Jadwal Presensi Hari Ini")
+                            .font(.caption)
+                        
+                        Text("Jumat, 07:30 - 16:00 WIB")
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Presensi")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .frame(height: 30)
+                            .padding(.horizontal, 20)
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(.green)
+                            )
+                    }
+
+                }
             }
             
+            // Account Summary section
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.white)
+                    .frame(height: 150)
+            }
         }
+        .padding(.top, 5)
     }
 }
 
