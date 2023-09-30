6d83b2a56721cb2d2a5ad2dc1e6af43659f6b96d
Agung Nawawi <agung@fairtech.com.sg>
2023-09-30 20:24:24 +0700

    top menu sijawara

--- SwiftUIBootcamp/SijawaraDashboard.swift
+++ SwiftUIBootcamp/SijawaraDashboard.swift
@@ -13,12 +13,61 @@ struct SijawaraDashboard: View {
             Color.teal
                 .ignoresSafeArea()
             VStack {
-                Text("SSSS")
+                topMenu()
+                
+                accountSummary()
                 
                 Spacer()
                 
                 Text("ajhkhk")
             }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+
+// top menu
+struct topMenu: View {
+    var body: some View {
+        
+        let kolom: [GridItem] = [
+            GridItem(.fixed(75), spacing: nil, alignment: .leading),
+            GridItem(.flexible(), spacing: 5, alignment: .center),
+            GridItem(.fixed(75), spacing: nil, alignment: .trailing)
+        ]
+        
+        VStack {
+            LazyVGrid(columns: kolom){
+                Image(systemName: "text.justify")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Text("SIJAWARA")
+                    .font(.system(size: 24))
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+                
+                Image(systemName: "arrow.clockwise")
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+
+//  presensi dan account summary
+struct accountSummary: View {
+    var body: some View {
+        VStack {
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.white)
+                    .frame(height: 75)
+            }
+            
         }
     }
 }
