9e6d722ab8bc62e69cc6ecb29b78b9b5531ac479
Agung Nawawi <agung@fairtech.com.sg>
2023-10-04 10:04:58 +0700

    Conditional View

---
+++ SwiftUIBootcamp/Conditional/ConditionalsView.swift
@@ -0,0 +1,18 @@
+//
+//  ConditionalsView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 04/10/23.
+//
+
+import SwiftUI
+
+struct ConditionalsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConditionalsView()
+}
