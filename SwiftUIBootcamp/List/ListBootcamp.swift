e36a03febaf3f8825b128d7728cdbea918fedddf
Agung Nawawi <agung@fairtech.com.sg>
2023-10-10 14:28:49 +0700

    create custom list row, header

--- SwiftUIBootcamp/List/ListBootcamp.swift
+++ SwiftUIBootcamp/List/ListBootcamp.swift
@@ -12,31 +12,72 @@ struct ListBootcamp: View {
     @State var fruits: [String] = [
         "Apple", "Banana", "Grape", "Peach", "Mango"
     ]
+    @State var veggies: [String] = [
+        "Zucchini",
+        "Cherry",
+        "Green beans",
+        "Asparagus",
+        "Cherry tomatoes",
+        "Baby corn",
+        "Snap peas"
+    ]
     
     var body: some View {
         NavigationView {
             List {
+                // # frruits section
                 Section {
                     ForEach(fruits, id: \.self) { item in
                         Text(item)
+                            .foregroundStyle(Color.white)
                     }
                     // salah satu fitur yang powerful di list adalah dia bisa membuat/hapus list item didalam list
                     
                     // buat func diluar. untuk membuat clean code
                     .onDelete (perform: delete)
                     .onMove(perform: move)
+                    .listRowBackground(Color.gray)
                     
                 } header: {
-                    Text("Fruits")
-                        .font(.title)
+                    HStack {
+                        Text("Fruits")
+                        Image(systemName: "flame.fill")
+                    }
+                    .font(.headline)
+                    .foregroundStyle(.blue)
                 }
+                
+                // # Veggiees section
+                Section(content: {
+                    ForEach(veggies, id:\.self) { item in
+                        Text(item.capitalized)
+                            .font(.subheadline)
+                    }
+                }, header: {
+                    Text("Vegetables")
+                        .font(.headline)
+                        .foregroundStyle(.blue)
+                })
+
             }
+            .listStyle(InsetGroupedListStyle())
             .navigationTitle("Grocery List")
             .toolbar(content: {
                 ToolbarItem(placement: .topBarTrailing) {
                     EditButton()
                 }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    addButton
+                }
             })
+            .tint(.red)
+        }
+    }
+    
+    var addButton: some View {
+        Button("Add") {
+            add()
         }
     }
     
@@ -48,6 +89,11 @@ struct ListBootcamp: View {
     // buat func move. untuk membuat clean code
     func move(indices: IndexSet, newOffset: Int) {
         fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    // func addd
+    func add() {
+        fruits.append("Melon ")
     }
 }
 
