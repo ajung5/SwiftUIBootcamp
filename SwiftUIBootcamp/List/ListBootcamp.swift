458128c8ff5345f3ebcb5543dc5237d058c40595
Agung Nawawi <agung@fairtech.com.sg>
2023-10-10 13:41:34 +0700

    buat edit button dan move button

--- SwiftUIBootcamp/List/ListBootcamp.swift
+++ SwiftUIBootcamp/List/ListBootcamp.swift
@@ -14,20 +14,40 @@ struct ListBootcamp: View {
     ]
     
     var body: some View {
-        List {
-            Section {
-                ForEach(fruits, id: \.self) { item in
-                    Text(item)
+        NavigationView {
+            List {
+                Section {
+                    ForEach(fruits, id: \.self) { item in
+                        Text(item)
+                    }
+                    // salah satu fitur yang powerful di list adalah dia bisa membuat/hapus list item didalam list
+                    
+                    // buat func diluar. untuk membuat clean code
+                    .onDelete (perform: delete)
+                    .onMove(perform: move)
+                    
+                } header: {
+                    Text("Fruits")
+                        .font(.title)
                 }
-                // salah satu fitur yang powerful di list adalah dia bisa membuat/hapus list item didalam list
-                .onDelete(perform: { indexSet in
-                    fruits.remove(atOffsets: indexSet)
-                })
-            } header: {
-                Text("Fruits")
-                    .font(.title)
             }
+            .navigationTitle("Grocery List")
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarTrailing) {
+                    EditButton()
+                }
+            })
         }
+    }
+    
+    // buat func delete. untuk membuat clean code
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+    }
+    
+    // buat func move. untuk membuat clean code
+    func move(indices: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
     }
 }
 
