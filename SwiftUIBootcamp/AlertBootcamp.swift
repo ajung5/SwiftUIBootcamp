984c972fd032d6d84e332dfcbc56f968f1f65859
Agung Nawawi <agung@fairtech.com.sg>
2023-10-10 21:59:17 +0700

    add alert

---
+++ SwiftUIBootcamp/AlertBootcamp.swift
@@ -0,0 +1,40 @@
+//
+//  AlertBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 10/10/23.
+//
+
+import SwiftUI
+
+struct AlertBootcamp: View {
+    
+    @State var showAlert: Bool = false
+    @State var backgroundColor: Color = Color.teal
+    @State var foregroundColor: Color = .white
+    
+    var body: some View {
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            
+            Button("Click Me") {
+                showAlert.toggle()
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Ini Judul"),
+                      message: Text("Penjelasan Error"),
+                      primaryButton: .destructive(Text("Hapus"),
+                                                  action: {
+                    backgroundColor = .vividCerise
+                    //foregroundColor = .white
+                }),
+                      secondaryButton: .cancel())
+            }
+        }
+    }
+}
+
+#Preview {
+    AlertBootcamp()
+}
