43ee7ba44fcb01c5fdeff51ef0fa7d0853136781
Agung Nawawi <agung@fairtech.com.sg>
2023-10-10 22:02:10 +0700

    Extracting alert

--- SwiftUIBootcamp/AlertBootcamp.swift
+++ SwiftUIBootcamp/AlertBootcamp.swift
@@ -22,16 +22,21 @@ struct AlertBootcamp: View {
                 showAlert.toggle()
             }
             .alert(isPresented: $showAlert) {
-                Alert(title: Text("Ini Judul"),
-                      message: Text("Penjelasan Error"),
-                      primaryButton: .destructive(Text("Hapus"),
-                                                  action: {
-                    backgroundColor = .vividCerise
-                    //foregroundColor = .white
-                }),
-                      secondaryButton: .cancel())
+                getAlert()
             }
         }
+    }
+    
+    // func alert
+    func getAlert() -> Alert {
+        Alert(title: Text("Ini Judul"),
+              message: Text("Penjelasan Error"),
+              primaryButton: .destructive(Text("Ok"),
+                                          action: {
+            backgroundColor = .vividCerise
+            //foregroundColor = .white
+        }),
+              secondaryButton: .cancel())
     }
 }
 
