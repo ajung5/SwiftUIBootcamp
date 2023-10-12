8aeb409d9f308dd1f53cb4a4e9c58fb1e094215e
Agung Nawawi <agung@fairtech.com.sg>
2023-10-12 14:37:34 +0700

    create coonfirmation dialogue

---
+++ SwiftUIBootcamp/ActionSheetBootcamp.swift
@@ -0,0 +1,72 @@
+//
+//  ActionSheetBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 12/10/23.
+//
+
+import SwiftUI
+
+struct ActionSheetBootcamp: View {
+    
+    @State var isShowingDialog: Bool = false
+    @State var actionSheetOption: ActionSheetOptions = .isOtherPost
+    
+    enum ActionSheetOptions {
+        case isMyPost
+        case isOtherPost
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Circle()
+                    .frame(width: 30, height: 30)
+                Text("@username")
+                Spacer()
+                Button(action: {
+                    actionSheetOption = .isMyPost
+                    isShowingDialog.toggle()
+                }, label: {
+                    Image(systemName: "ellipsis")
+                })
+                .tint(.primary)
+            }
+            Rectangle()
+                .aspectRatio(1.0, contentMode: .fit)
+        }
+        .padding()
+        .confirmationDialog("Are You Sure?",
+                            isPresented: $isShowingDialog) {
+            switch actionSheetOption {
+            case .isOtherPost:
+                Button("Share") {
+                    
+                }
+                
+                Button("Report", role: .destructive) {
+                    
+                }
+                
+            case .isMyPost:
+                Button("Share") {
+                    
+                }
+                
+                Button("Report", role: .destructive) {
+                    
+                }
+                
+                Button("Delete", role: .destructive) {
+                    
+                }
+            }
+        } message: {
+            Text("You Cannot undo this action")
+        }
+    }
+}
+
+#Preview {
+    ActionSheetBootcamp()
+}
