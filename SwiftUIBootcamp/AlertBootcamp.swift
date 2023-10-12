1d9492f99f5b5af5af37e172ebeeec7f48f237b1
Agung Nawawi <agung@fairtech.com.sg>
2023-10-12 11:07:49 +0700

    Custom Allert #1

--- SwiftUIBootcamp/AlertBootcamp.swift
+++ SwiftUIBootcamp/AlertBootcamp.swift
@@ -13,13 +13,28 @@ struct AlertBootcamp: View {
     @State var backgroundColor: Color = Color.teal
     @State var foregroundColor: Color = .white
     
+    // Custom Alert #1
+    // untuk membuat title dan message alert menjadi dinamic, maka perlu membuat variable untuk menampungnya
+    // jika menggunakan meetode ini, ketika akan mengubah isi dari variabel tsb, maka semua isi var tsb harus di ubah juga, agar bisa tampil isi yang baru
+    @State var alertTitle: String = ""
+    @State var alertMessage: String = ""
+    
     var body: some View {
         ZStack {
             backgroundColor
                 .ignoresSafeArea()
             
-            Button("Click Me") {
-                showAlert.toggle()
+            VStack {
+                Button("Button 1") {
+                    alertTitle = "Error Euy"
+                    alertMessage = "The Video Could Not be Uploaded"
+                    showAlert.toggle()
+                }
+                Button("Button 2") {
+                    alertTitle = "Succesfully Uploading Video"
+                    alertMessage = "Your video now public"
+                    showAlert.toggle()
+                }
             }
             .alert(isPresented: $showAlert) {
                 getAlert()
@@ -29,14 +44,18 @@ struct AlertBootcamp: View {
     
     // func alert
     func getAlert() -> Alert {
-        Alert(title: Text("Ini Judul"),
-              message: Text("Penjelasan Error"),
-              primaryButton: .destructive(Text("Ok"),
-                                          action: {
-            backgroundColor = .vividCerise
-            //foregroundColor = .white
-        }),
-              secondaryButton: .cancel())
+//        return Alert(title: Text("Ini Judul"),
+//              message: Text("Penjelasan Error"),
+//              primaryButton: .destructive(Text("Ok"),
+//                                          action: {
+//            backgroundColor = .vividCerise
+//            //foregroundColor = .white
+//        }),
+//              secondaryButton: .cancel())
+        
+        return Alert(title: Text(alertTitle),
+                     message: Text(alertMessage),
+                     dismissButton: .default(Text("OK")))
     }
 }
 
