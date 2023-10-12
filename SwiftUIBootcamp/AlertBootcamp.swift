fa0d05f82dcdb0b751bb634efbbfb385b7d43ece
Agung Nawawi <agung@fairtech.com.sg>
2023-10-12 11:19:32 +0700

    Custom error #2 using enum

--- SwiftUIBootcamp/AlertBootcamp.swift
+++ SwiftUIBootcamp/AlertBootcamp.swift
@@ -16,8 +16,16 @@ struct AlertBootcamp: View {
     // Custom Alert #1
     // untuk membuat title dan message alert menjadi dinamic, maka perlu membuat variable untuk menampungnya
     // jika menggunakan meetode ini, ketika akan mengubah isi dari variabel tsb, maka semua isi var tsb harus di ubah juga, agar bisa tampil isi yang baru
-    @State var alertTitle: String = ""
-    @State var alertMessage: String = ""
+    // @State var alertTitle: String = ""
+    // @State var alertMessage: String = ""
+    
+    // Custom Alert #2
+    @State var alertType: MyAlerts? = nil
+    
+    enum MyAlerts {
+        case success
+        case error
+    }
     
     var body: some View {
         ZStack {
@@ -26,15 +34,20 @@ struct AlertBootcamp: View {
             
             VStack {
                 Button("Button 1") {
-                    alertTitle = "Error Euy"
-                    alertMessage = "The Video Could Not be Uploaded"
+//                    alertTitle = "Error Euy"
+//                    alertMessage = "The Video Could Not be Uploaded"
+                    alertType = .error
                     showAlert.toggle()
                 }
+                .tint(.white)
+                
                 Button("Button 2") {
-                    alertTitle = "Succesfully Uploading Video"
-                    alertMessage = "Your video now public"
+//                    alertTitle = "Succesfully Uploading Video"
+//                    alertMessage = "Your video now public"
+                    alertType = .success
                     showAlert.toggle()
                 }
+                .tint(.white)
             }
             .alert(isPresented: $showAlert) {
                 getAlert()
@@ -53,9 +66,24 @@ struct AlertBootcamp: View {
 //        }),
 //              secondaryButton: .cancel())
         
-        return Alert(title: Text(alertTitle),
-                     message: Text(alertMessage),
-                     dismissButton: .default(Text("OK")))
+//        return Alert(title: Text(alertTitle),
+//                     message: Text(alertMessage),
+//                     dismissButton: .default(Text("OK")))
+        
+        switch alertType {
+        case .error:
+            return Alert(title: Text("Ada Error"))
+            
+        case .success:
+            return Alert(title: Text("Succes"),
+                         message: Text("Success euy"),
+                         dismissButton: .default(Text("OK"), action: {
+                backgroundColor = .gray
+            }))
+            
+        default:
+            return Alert(title: Text("ERROR"))
+        }
     }
 }
 
