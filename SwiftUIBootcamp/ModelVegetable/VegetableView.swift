7c61dc81ef5ac0b8982c8950241754a32762af35
Agung Nawawi <agung@fairtech.com.sg>
2023-10-18 14:03:39 +0700

    Awal mula sebelum pake @StateObject

--- SwiftUIBootcamp/ModelVegetable/VegetableView.swift
+++ SwiftUIBootcamp/ModelVegetable/VegetableView.swift
@@ -12,9 +12,13 @@
 import SwiftUI
 
 struct VegetableView: View {
+    
+    @State var veggieArray: [VegetableModel] = []
+    
     var body: some View {
         NavigationStack {
             List {
+                // melakukan perulangan pada variabel "veggieArray" yang beertipe data "VegetableModel"
                 ForEach(veggieArray) { item in
                     HStack {
                         Text("\(item.count)")
@@ -25,13 +29,28 @@ struct VegetableView: View {
                             .bold()
                     }
                 }
+                .onDelete(perform: deleteVeggie)
             }
             .listStyle(GroupedListStyle())
             .navigationTitle("Veggie List")
             .onAppear{
-                
+                getVeggies()
             }
         }
+    }
+    
+    func getVeggies() {
+        let veggie1 = VegetableModel(name: "Peas", count: 2)
+        let veggie2 = VegetableModel(name: "Mushroom", count: 6)
+        let veggie3 = VegetableModel(name: "Kale", count: 7)
+        
+        veggieArray.append(veggie1)
+        veggieArray.append(veggie2)
+        veggieArray.append(veggie3)
+    }
+    
+    func deleteVeggie(index: IndexSet) {
+        veggieArray.remove(atOffsets: index)
     }
 }
 
--- SwiftUIBootcamp/ModelVegetable/VeggieModel.swift
+++ SwiftUIBootcamp/ModelVegetable/VeggieModel.swift
@@ -15,17 +15,27 @@ struct VegetableModel: Identifiable {
     let count: Int
 }
 
-var veggieArray: [VegetableModel] = [
+var veggiesArray: [VegetableModel] = [
     VegetableModel(name: "Tomatoes", count: 4),
     VegetableModel(name: "Cucumbers", count: 6),
     VegetableModel(name: "Bell peppers", count: 8),
     VegetableModel(name: "Zucchini", count: 2),
     VegetableModel(name: "Eggplants", count: 1),
+    
     VegetableModel(name: "Broccoli", count: 3),
     VegetableModel(name: "Cauliflower", count: 5),
     VegetableModel(name: "Spinach", count: 7),
-    VegetableModel(name: "Lettuce", count: 9),
-    VegetableModel(name: "Kale", count: 10),
-    VegetableModel(name: "Mushroom", count: 14),
-    VegetableModel(name: "Peas", count: 2),
+    VegetableModel(name: "Lettuce", count: 9)
+    
 ]
+
+
+func getVeggies() {
+    let veggie1 = VegetableModel(name: "Peas", count: 2)
+    let veggie2 = VegetableModel(name: "Mushroom", count: 6)
+    let veggie3 = VegetableModel(name: "Kale", count: 7)
+    
+    veggiesArray.append(veggie1)
+    veggiesArray.append(veggie2)
+    veggiesArray.append(veggie3)
+}
