ef7ea54ac98382b4a3a052107236c775ba09a4a5
Agung Nawawi <agung@fairtech.com.sg>
2023-10-19 20:18:59 +0700

    Property Wrapper - @State

---
+++ SwiftUIBootcamp/ProopertyWrapper/StateWrapperView.swift
@@ -0,0 +1,18 @@
+//
+//  StateWrapperView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 19/10/23.
+//
+
+import SwiftUI
+
+struct StateWrapperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StateWrapperView()
+}
