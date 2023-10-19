41ee1a1b16948ca92f261eda62c2457a4ce790eb
Agung Nawawi <agung@fairtech.com.sg>
2023-10-19 21:55:42 +0700

    Property Wrapper - @State

--- SwiftUIBootcamp/PropertyWrapper/StateWrapperView.swift
+++ SwiftUIBootcamp/PropertyWrapper/StateWrapperView.swift
@@ -5,11 +5,34 @@
 //  Created by Agung Nawawi on 19/10/23.
 //
 
+
+// @State allows you to create properties that can be changed and, if necessary, update the interface based on these changes.
+// For example, if you want to create a button that changes its color when pressed, you can create a @State variable to store the color and add it to the button
 import SwiftUI
 
 struct StateWrapperView: View {
+    
+    @State var backgroundColor: Color = .teal
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundColor
+                .ignoresSafeArea()
+            
+            VStack {
+                Button(action: {
+                    backgroundColor = Color.erieBlack
+                }, label: {
+                    Text("Press")
+                        .tint(.erieBlack)
+                        //.frame(width: 100, height: 100)
+                        .padding()
+                        .background(Color.jasmine)
+                        .clipShape(Capsule())
+                })
+            }
+
+        }
     }
 }
 
