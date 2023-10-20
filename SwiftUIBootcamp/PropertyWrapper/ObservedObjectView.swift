3b6d3cbd9d998ef669c1ef7f1ca1c682e3361737
Agung Nawawi <agung@fairtech.com.sg>
2023-10-20 20:05:34 +0700

    cc

--- SwiftUIBootcamp/PropertyWrapper/ObservedObjectView.swift
+++ SwiftUIBootcamp/PropertyWrapper/ObservedObjectView.swift
@@ -28,6 +28,17 @@ struct ObservedObjectView: View {
     }
 }
 
+/*
+ In this example, we create a class called UserData, which contains a @Published name. In the ContentView structure, we create a property called userData with the type UserData, using @ObservedObject. We display the value of userData.name in a text field and output it on the screen.
+
+
+ When the user changes the value in the text field, SwiftUI automatically updates the corresponding part of the interface, as the name property is published and observed using @Published. This means we don't need our own code to update the interface, and we allow SwiftUI to do it for us.
+
+
+ Note: If you don't know, @Published is a property wrapper from the Combine framework that can be added to a class or structure property, which automatically sends notifications of any changes to the value of that property to anyone who has subscribed to it. In other words, it's a helper attribute for properties that can be tracked for changes.
+ 
+ */
+
 #Preview {
     ObservedObjectView()
 }
