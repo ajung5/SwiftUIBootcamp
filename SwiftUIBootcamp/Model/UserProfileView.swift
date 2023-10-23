cde9351236d3a43ab389342bb83f28677e8b77eb
Agung Nawawi <agung@fairtech.com.sg>
2023-10-23 09:19:55 +0700

    edit user model

--- SwiftUIBootcamp/Model/UserProfileView.swift
+++ SwiftUIBootcamp/Model/UserProfileView.swift
@@ -9,10 +9,65 @@ import SwiftUI
 
 struct UserProfileView: View {
     
+    @State var isShowingDialog: Bool = false
+    @State var actionSheetOption: ActionSheetOptions = .isOtherPost
+    
+    enum ActionSheetOptions {
+        case isMyPost
+        case isOtherPost
+    }
+    
     var user: PersonModel
     
     var body: some View {
-        Text(user.firstName)
+            VStack {
+                HStack {
+                    Circle()
+                        .frame(width: 30, height: 30)
+                    Text(user.userName)
+                    Spacer()
+                    Button(action: {
+                        actionSheetOption = .isMyPost
+                        isShowingDialog.toggle()
+                    }, label: {
+                        Image(systemName: "ellipsis")
+                    })
+                    .tint(.primary)
+                }
+                Rectangle()
+                    .aspectRatio(1.0, contentMode: .fit)
+            }
+            .padding()
+            .confirmationDialog("Are You Sure?",
+                                isPresented: $isShowingDialog) {
+                
+                switch actionSheetOption {
+                case .isOtherPost:
+                    Button("Share") {
+                        
+                    }
+                    
+                    Button("Report", role: .destructive) {
+                        
+                    }
+                    
+                case .isMyPost:
+                    Button("Share") {
+                        
+                    }
+                    
+                    Button("Report", role: .destructive) {
+                        
+                    }
+                    
+                    Button("Delete", role: .destructive) {
+                        
+                    }
+                }
+            } message: {
+                Text("You Cannot undo this action")
+            }
+        
     }
 }
 
