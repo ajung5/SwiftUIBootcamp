d03ee188f3ce7dcdde8c2cac4a4e0b5cf5bf887b
Agung Nawawi <agung@fairtech.com.sg>
2023-10-24 21:35:33 +0700

    # Perbedaan @State, @StateObject dan @ObservedObject

--- SwiftUIBootcamp/ObservableAndStateObject/CitizenView.swift
+++ SwiftUIBootcamp/ObservableAndStateObject/CitizenView.swift
@@ -22,7 +22,11 @@ struct CitizenView: View {
     
     // variabel untuk mengakses array yang ada didalam class CitizenManager
     // untuk membuat view mengupdate data dari class tersebut, perlu ditambahkan property wrapper pada variabel "citizenManager"
-    @ObservedObject var citizenManager: CitizenManager = CitizenManager()
+    //    @ObservedObject var citizenManager: CitizenManager = CitizenManager()
+    
+    // @StateObbject     -> USE THIS ON CREATIOON / INIT
+    // @ObservedObject   -> USE THIS FOR SUBVIEWS
+    @StateObject var citizenManager: CitizenManager = CitizenManager()
     
     var body: some View {
         NavigationStack {
@@ -56,3 +60,14 @@ struct CitizenView: View {
 #Preview {
     CitizenView()
 }
+
+// # Perbedaan @State, @StateObject dan @ObservedObject
+
+// @State
+// @State is a property wrapper that allows a value to be stored and managed by a view. It is typically used for simple, localized state within a view hierarchy. Whenever the value assigned to a @State property changes, the view is automatically re-rendered. However, it's important to note that @State is only designed for use within a single view and cannot be shared across multiple views
+
+// @StateObject
+// @StateObject is another property wrapper that is used to store and manage an object's state within a view. It is specifically designed for reference types (classes) and is recommended for managing the lifecycle of observable objects. @StateObject is used when the object's lifecycle is tightly bound to the view's lifecycle. The object is created once and persisted across view updates.
+
+// @ObservedObject
+// @ObservedObject is used to bind an observable object to a view. It is typically used when a view needs to observe and react to changes in an external object's state. Unlike @StateObject, the lifecycle of the observed object is not managed by the view. Instead, the observed object is created and passed from the parent view.
