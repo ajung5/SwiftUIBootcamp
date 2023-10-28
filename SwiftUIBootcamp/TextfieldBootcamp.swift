b2d21dcfcb1bfad955083dcf247c38d5d5da8479
Agung Nawawi <agung@fairtech.com.sg>
2023-10-28 22:35:50 +0700

    Text Field

--- SwiftUIBootcamp/TextfieldBootcamp.swift
+++ SwiftUIBootcamp/TextfieldBootcamp.swift
@@ -6,10 +6,69 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct TextfieldBootcamp: View {
+    @State var textFieldText: String = ""
+    @State var dataArray: [String] = []
+    @State var showAlert: Bool = false
+    @State var alertTitle: String = ""
+    @State var alertMessage: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                TextField("Tulis sesuatu disini", text: $textFieldText)
+                    .padding()
+                    .background(.gray.opacity(0.3))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .foregroundStyle(.red)
+                    .font(.headline)
+                
+                Button(action: {
+                    if textValidation() {
+                        saveText()
+                    }
+//                    } else {
+//                        alertTitle = "Succesfully Uploading Video"
+//                        alertMessage = "Your video now public"
+//                        //alertType = nil
+//                        showAlert.toggle()
+//                    }
+                }, label: {
+                    Text("Save".uppercased())
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(textValidation() ? .blue : .gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                })
+               // .disabled(!textValidation())
+                
+                // area untuk menampilkan text/data yang di inputkan
+                ForEach(dataArray, id: \.self) { data in
+                    Text(data)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Text Field")
+        }
+    }
+    
+    // function
+    func saveText() {
+        dataArray.append(textFieldText)
+    }
+    
+    func textValidation() -> Bool {
+        // check text
+        if textFieldText.count >= 3 {
+            return true
+        }
+        return false
     }
 }
 
