c8318f7ec7914ade1a121e57030ab68470a5a6b1
Agung Nawawi <agung@fairtech.com.sg>
2023-10-28 21:34:54 +0700

    Text Field Init

---
+++ SwiftUIBootcamp/TextfieldBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TextfieldBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 28/10/23.
+//
+
+import SwiftUI
+
+struct TextfieldBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextfieldBootcamp()
+}
