08a6e26ab0e13a6cfb530c66a68832c7cd2a2a9b
Agung Nawawi <agung@fairtech.com.sg>
2023-11-27 14:54:06 +0700

    Menambahkan halaman profil

---
+++ SwiftUIBootcamp/OnboardingViews/HalamanProfil.swift
@@ -0,0 +1,62 @@
+//
+//  HalamanProfil.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 27/11/23.
+//
+
+import SwiftUI
+
+struct HalamanProfil: View {
+    // for appstorage
+    @AppStorage("age") var currentUserAge: Int?
+    @AppStorage("gender") var currentUserGender: String?
+    @AppStorage("name") var currentUsername: String?
+    @AppStorage("signed_in") var currentUserSignedIn: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            
+            Text(currentUsername ?? "Your Name Here")
+            Text("Your Age is \(currentUserAge ?? 0) Years Old")
+            Text("Your Gender is \(currentUserGender ?? "Unknown")")
+            
+            Text("SIGN OUT")
+                .foregroundStyle(.white)
+                .font(.headline)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 10)
+                )
+                .frame(height: 55)
+                .frame(maxWidth: .infinity)
+                .background(Color.black)
+                .onTapGesture {
+                    signOut()
+                }
+        }
+        .foregroundStyle(.purple)
+        .font(.title)
+        .padding()
+        .padding(.vertical, 40)
+        .shadow(radius: 10)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .background(Color.white)
+    }
+    
+    func signOut() {
+        currentUsername = nil
+        currentUserAge = nil
+        currentUserGender = nil
+        withAnimation(.spring()) {
+            currentUserSignedIn = false
+        }
+    }
+}
+
+#Preview {
+    HalamanProfil()
+}
--- SwiftUIBootcamp/OnboardingViews/IntroView.swift
+++ SwiftUIBootcamp/OnboardingViews/IntroView.swift
@@ -29,7 +29,7 @@ struct IntroView: View {
             // else -> onboarding View
             
             if currentUserSignedIn {
-                Text("Profile View")
+                HalamanProfil()
             } else {
                 OnboardingView() 
             }
