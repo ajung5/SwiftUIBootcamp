ab994d1d0b438a6090d4bfc28f74a54094de6357
Agung Nawawi <agung@fairtech.com.sg>
2023-11-29 10:10:43 +0700

    Background Material

---
+++ SwiftUIBootcamp/BackgorundMaterialBootcamp.swift
@@ -0,0 +1,44 @@
+//
+//  BackgorundMaterialBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 29/11/23.
+//
+
+import SwiftUI
+
+struct BackgorundMaterialBootcamp: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            VStack {
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 50, height: 4)
+                    .padding()
+                
+                
+                Text("Hello, World!")
+                
+                Spacer()
+                Spacer()
+            }
+            .frame(height: 350)
+            .frame(maxWidth: .infinity)
+            
+            // to blur the bbackground
+            .background(.thinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+        }
+        .ignoresSafeArea()
+        .background(
+            Image("github")
+                .resizable()
+                .scaledToFit()
+        )
+    }
+}
+
+#Preview {
+    BackgorundMaterialBootcamp()
+}
