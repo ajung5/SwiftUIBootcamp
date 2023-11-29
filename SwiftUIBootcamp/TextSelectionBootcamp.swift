6bdd56d6b5581fa121373bae9e0aa34d889d6ace
Agung Nawawi <agung@fairtech.com.sg>
2023-11-29 10:15:03 +0700

    text selection

---
+++ SwiftUIBootcamp/TextSelectionBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 29/11/23.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBootcamp()
+}
