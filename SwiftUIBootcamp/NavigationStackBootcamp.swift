94608f734f7574688a3d8fda30040201b6945858
Agung Nawawi <agung@fairtech.com.sg>
2023-12-19 20:40:16 +0700

    Toolbar - placement

---
+++ SwiftUIBootcamp/NavigationStackBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationStackBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 01/12/23.
+//
+
+import SwiftUI
+
+struct NavigationStackBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStackBootcamp()
+}
---
+++ SwiftUIBootcamp/ToolbarBootcamp.swift
@@ -0,0 +1,37 @@
+//
+//  ToolbarBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 19/12/23.
+//
+
+import SwiftUI
+
+struct ToolbarBootcamp: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color.teal.ignoresSafeArea()
+                
+                Text("Kamikaze")
+                    .foregroundStyle(.primary)
+            }
+            .navigationTitle("Toolbar")
+            .font(.headline)
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Image(systemName: "house")
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "gear")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ToolbarBootcamp()
+}
