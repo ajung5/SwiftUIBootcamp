def4643ff05f0b275520894b964e274327209fc5
Agung Nawawi <agung@fairtech.com.sg>
2024-02-02 19:29:26 +0700

    Resizable sheet

---
+++ SwiftUIBootcamp/ResizableBootcamp.swift
@@ -0,0 +1,64 @@
+//
+//  ResizableBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Agung Nawawi on 02/02/24.
+//
+
+import SwiftUI
+
+struct ResizableBootcamp: View {
+    
+    @State private var showSheet: Bool = false
+    @State private var showSheet2: Bool = false
+    
+    var body: some View {
+        Button("Click Me!") {
+            showSheet.toggle()
+        }
+        .sheet(isPresented: $showSheet, content: {
+            MyNextView()
+                // detent adalah ukuran sheet yang akan ditampilkan
+            
+                // # single detent
+                // .presentationDetents([.medium])
+            
+                // # double detent, untuk membuat dua ukuran detent
+                .presentationDetents([.medium, .large])
+                .presentationDragIndicator(.hidden)
+        })
+        
+        Button("Click Me 2!") {
+            showSheet2.toggle()
+        }
+        .sheet(isPresented: $showSheet2, content: {
+            NextView()
+                .presentationDetents([.medium])
+                .interactiveDismissDisabled() // mencegah user untuk mengembalikan ke state semula
+        })
+    }
+}
+
+struct MyNextView: View {
+    var body: some View {
+        ZStack {
+            Color.teal.ignoresSafeArea()
+            Text("Hello World!")
+        }
+    }
+    
+}
+
+struct NextView: View {
+    var body: some View {
+        ZStack {
+            Color.pink.ignoresSafeArea()
+            Text("Hello World!")
+        }
+    }
+    
+}
+
+#Preview {
+    ResizableBootcamp()
+}
