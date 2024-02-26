0ec2bf7f3c081391c1cfdf1a49e81a984edbece7
Agung Nawawi <agung@fairtech.com.sg>
2024-02-26 14:43:20 +0700

    Complex Grid

--- SwiftUIBootcamp/Grid/GridViewBootcamp.swift
+++ SwiftUIBootcamp/Grid/GridViewBootcamp.swift
@@ -9,24 +9,38 @@ import SwiftUI
 
 struct GridViewBootcamp: View {
     var body: some View {
-        Grid {
-            GridRow {
-                Text("1")
-                Text("1")
-                Text("1")
+        Grid(alignment: .center, horizontalSpacing: 10, verticalSpacing: 10) {
+            ForEach(0..<4) { rowIndex in
+                GridRow(alignment: .bottom) {
+                    ForEach(0..<4) { columnIndex in
+                        let cellNumber = (rowIndex * 4) + (columnIndex + 1)
+                        
+                        if cellNumber == 7 {
+                             EmptyView()
+//                            Color.green
+//                                .gridCellUnsizedAxes([.horizontal, .vertical])
+                        } else {
+                            cell(num: cellNumber)
+                                .gridCellColumns(cellNumber == 6 ? 2 : 1)
+                                .gridCellAnchor(.leading)
+                                .gridColumnAlignment(
+                                    cellNumber == 1 ? .trailing :
+                                    cellNumber == 4 ? .leading :
+                                    .center
+                                )
+                        }
+                    }
+                }
             }
-            GridRow {
-                Text("2")
-                Text("2")
-                Text("2")
-            }
-            Text("1")
-            Text("2")
-            Text("3")
-            Text("4")
-            Text("6")
-            Text("7")
         }
+    }
+    
+    private func cell(num: Int) -> some View {
+        Text("\(num)")
+            .frame(height: num == 10 ? 20 : nil)
+            .font(.largeTitle)
+            .padding()
+            .background(Color.teal)
     }
 }
 
